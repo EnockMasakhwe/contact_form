0174ec19ce57cbcc2769c2827e3e9537f6436188
EnockMasakhwe <enockmasakhwe@gmail.com>
2026-03-31 14:35:47 +0300

    Added loggers to the service layer to notify user when data reaches backend

--- src/main/java/com/eliarojr/contact_form/service/ContactMessageServiceImpl.java
+++ src/main/java/com/eliarojr/contact_form/service/ContactMessageServiceImpl.java
@@ -2,6 +2,8 @@ package com.eliarojr.contact_form.service;
 
 import com.eliarojr.contact_form.entity.ContactMessage;
 import com.eliarojr.contact_form.repository.ContactMessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,14 @@ public class ContactMessageServiceImpl implements ContactMessageService {
     @Autowired
     private ContactMessageRepository contactMessageRepository;
 
+    private final Logger log = LoggerFactory.getLogger(ContactMessageServiceImpl.class);
+
+
     @Override
     public ContactMessage saveMessage(ContactMessage contactMessage) {
         contactMessage.setCreatedAt(LocalDateTime.now());
         contactMessage.setStatus("NEW");
+        log.info("Saving message from: {}", contactMessage.getEmail());
         return contactMessageRepository.save(contactMessage);
     }
 }
